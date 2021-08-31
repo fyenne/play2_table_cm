e52dee02ed8b03e1dc389ad1bb98a2b05240a913
fyenne <fyenne@hotmail.com>
2021-08-31 09:30:44 +0800

    add

--- sd_script_merge_table.sh
+++ sd_script_merge_table.sh
@@ -2,4 +2,4 @@
 # pyspark python3 py2 
 export PYSPARK_PYTHON="/app/anaconda3/bin/python"
 export PYSPARK_DRIVER_PYTHON="/app/anaconda3/bin/python"
-spark-submit --master yarn --deploy-mode client --driver-memory 8G --executor-memory 8G  --conf spark.dynamicAllocation.enabled=true --conf spark.dynamicAllocation.minExecutors=5 --conf spark.dynamicAllocation.maxExecutors=30 --conf spark.dynamicAllocation.initialExecutors=8 --conf spark.shuffle.service.enabled=true dsc ./sd_script_merge_table.py --tables  ${tables} 
+spark-submit --master yarn --deploy-mode client --driver-memory 8G --executor-memory 8G  --conf spark.dynamicAllocation.enabled=true --conf spark.dynamicAllocation.minExecutors=5 --conf spark.dynamicAllocation.maxExecutors=30 --conf spark.dynamicAllocation.initialExecutors=8 --conf spark.shuffle.service.enabled=true --queue root.dsc ./sd_script_merge_table.py --tables  ${tables} 
